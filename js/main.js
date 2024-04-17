e0c8b8feffe24da64ce11e67df36387cf2abb7da
mykola-lp <mykola.sukhorab@gmail.com>
2024-04-17 14:31:23 +0300

    Add side bar menu.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ document.querySelector('.search a').addEventListener('click', function() {
     let searchTop = document.querySelector('.search-top');
     searchTop.classList.toggle('active');
 });
+
+// Sidebar Menu.
+document.querySelector('.right-bar .bar').addEventListener('click', function() {
+    let sidebarMenu = document.querySelector('.sidebar-menu');
+    sidebarMenu.classList.add('active');
+});
+
+document.querySelector('.cross').addEventListener('click', function() {
+    let sidebarMenu = document.querySelector('.sidebar-menu');
+    sidebarMenu.classList.remove('active');
+});
